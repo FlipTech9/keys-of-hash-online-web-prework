d7921846f54defb72ad87e1643cb5e64c4d8d6ee
Alex S <alex.sunga@fliptech.co>
2018-12-19 11:51:25 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,11 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     collect.each do  |key, value|
-    if arguments.inclue?(value)
-      return key 
-    else 
-      return nil 
+      if arguments.inclue?(value)
+        return key 
+      else 
+        return nil 
+      end 
     end 
     
   end
