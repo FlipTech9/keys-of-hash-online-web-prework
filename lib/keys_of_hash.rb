bf853310c20701a1ab3034b001cc9c25f5e55a0e
Alex S <alex.sunga@fliptech.co>
2018-12-19 11:53:39 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
     # code goes here
     collect.each do  |key, value|
       if arguments.include?(value)
-        return key 
+        return key.compact 
       else 
         return nil 
       end 
